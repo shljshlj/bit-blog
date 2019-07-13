4892e09dbe107cdd8d86be6712b86304b3fd753b
shljshlj <34565607+shljshlj@users.noreply.github.com>
2019-07-13 20:56:51 +0200

    Updates

---
+++ precache-manifest.2ed265346f2b990e591c068161e8bbe4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45e8999c0740e671de63e71ad6931624",
+    "url": "/bit-blog/index.html"
+  },
+  {
+    "revision": "fa0707aed37f84abfae9",
+    "url": "/bit-blog/static/css/main.b4607a43.chunk.css"
+  },
+  {
+    "revision": "a57fadb27f1080911a88",
+    "url": "/bit-blog/static/js/2.51f5f5d1.chunk.js"
+  },
+  {
+    "revision": "fa0707aed37f84abfae9",
+    "url": "/bit-blog/static/js/main.6a5acf45.chunk.js"
+  },
+  {
+    "revision": "078ee378f4f9b1f6bd87",
+    "url": "/bit-blog/static/js/runtime~main.3d350344.js"
+  }
+]);
